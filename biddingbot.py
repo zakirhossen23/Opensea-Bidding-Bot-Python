d6d6ade403d270cdf87a696d11458e73291fc656
zakirhossen23 <zakirhoss12345@gmail.com>
2021-10-15 20:07:29 +0600

    metamask
    metamask

--- biddingbot.py
+++ biddingbot.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 from selenium import webdriver
-driver=webdriver.Chrome()
-driver.get("https://www.google.com")
+options = webdriver.ChromeOptions()
+options.add_extension("metamask.crx")
+driver=webdriver.Chrome(options=options)
+driver.switch_to.window(driver.window_handles["MetaMask"])
+
